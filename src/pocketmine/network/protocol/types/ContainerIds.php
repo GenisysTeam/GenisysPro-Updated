559ff89eecdefda839249691b30d142896b42e33
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-06 21:18:59 -0700

    Update ContainerIds.php

--- src/pocketmine/network/protocol/types/ContainerIds.php
+++ src/pocketmine/network/protocol/types/ContainerIds.php
@@ -4,15 +4,15 @@ namespace pocketmine\network\protocol\types;
 
 interface ContainerIds {
 
-    const NONE = -1;
-    const INVENTORY = 0;
-    const FIRST = 1;
-    const LAST = 100;
-    const OFFHAND = 119;
-    const ARMOR = 120;
-    const CREATIVE = 121;
-    const HOTBAR = 122;
-    const FIXED_INVENTORY = 123;
-    const UI = 124;
+	const NONE = -1;
+	const INVENTORY = 0;
+	const FIRST = 1;
+	const LAST = 100;
+	const OFFHAND = 119;
+	const ARMOR = 120;
+	const CREATIVE = 121;
+	const HOTBAR = 122;
+	const FIXED_INVENTORY = 123;
+	const UI = 124;
 
 }
