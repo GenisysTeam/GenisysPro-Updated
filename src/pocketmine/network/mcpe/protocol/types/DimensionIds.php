f78d74730fcf3e1193a778685d5ebeac2609e011
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-10-06 21:18:16 -0700

    Update DimensionIds.php

--- src/pocketmine/network/mcpe/protocol/types/DimensionIds.php
+++ src/pocketmine/network/mcpe/protocol/types/DimensionIds.php
@@ -5,8 +5,8 @@ namespace pocketmine\network\mcpe\protocol\types;
 
 interface DimensionIds {
 
-    const OVERWORLD = 0;
-    const NETHER = 1;
-    const END = 2;
+	const OVERWORLD = 0;
+	const NETHER = 1;
+	const END = 2;
 
 }
